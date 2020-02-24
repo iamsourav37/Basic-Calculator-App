3184c1fc42528e8e79e7e22dabd3ee55685b52ca
iamSourav37 <souravsrk120@gmail.com>
2020-02-24 17:32:43 +0530

    design complete and Addition method defined

--- app/src/main/java/com/example/basiccalculator/MainActivity.java
+++ app/src/main/java/com/example/basiccalculator/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.basiccalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etFirst,etSecond;
+    TextView tvResult;
+    Button btnAdd,btnSub,btnMult,btnDiv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etFirst = (EditText) findViewById(R.id.etFirstValue);
+        etSecond = (EditText)findViewById(R.id.etSecondValue);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub =(Button)  findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+
+    }
+
+    public void btnAdd(View view) {
+        int a = Integer.parseInt(etFirst.getText().toString());
+        int b = Integer.parseInt(etSecond.getText().toString());
+        int c = a+b;
+        tvResult.setText("Result is : "+c);
+
+
     }
 }
